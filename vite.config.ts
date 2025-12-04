3cc3efab0666de4012dc99140dcacf2198208bab
JenilKalariya <jenilkalariya2312@gmail.com>
2025-12-04 20:47:17 +0530

    inital

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -7,13 +8,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     build: {
       outDir: 'build',
-    },
-    define: {
-      // Expose the API_KEY to the client-side code
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-    },
+    }
   };
 });
